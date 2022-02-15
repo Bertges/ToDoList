1e6959f38bd4b3ab4cb3d238b20cc5b059224ba7
Bertges <felippe.amorim@terceiro-sky.com.br>
2022-02-15 20:56:58 -0300

    taskslistview

--- ToDoList/ToDoList/TasksList/Views/TasksListView.swift
+++ ToDoList/ToDoList/TasksList/Views/TasksListView.swift
@@ -5,4 +5,29 @@
 //  Created by Felippe Bertges on 15/02/22.
 //
 
-import Foundation
+import UIKit
+
+final class TasksListView: UIView, UITableViewDataSource {
+
+    lazy var addTaskButton: UIButton = {
+        let view: UIButton = .init(frame: .zero)
+        view.translatesAutoresizingMaskIntoConstraints = true
+        view.setTitle("Adicionar Tarefa", for: .normal)
+        return view
+    }()
+
+    lazy var tableView: UITableView = {
+        let view: UITableView = .init(frame: .zero)
+        view.translatesAutoresizingMaskIntoConstraints = true
+        view.dataSource = self
+        return view
+    }()
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell()
+    }
+}
